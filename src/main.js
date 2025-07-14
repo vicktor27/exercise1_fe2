744fab127a578ed837bde746c11954a1dd8b4368
Vickthor3T <victor.cardoza@tripleten-team.com>
2025-07-14 10:50:06 -0600

    chore: index bug fix

--- src/main.js
+++ src/main.js
@@ -1,25 +1,25 @@
-import "./style.css";
-import javascriptLogo from "./javascript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./css/sb-admin-2.min.css"; // or "./css/sb-admin-2.min.css"
 
-document.querySelector("#app").innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <h1>Victor Manuel Solis Cardoza. </h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
+import $ from "jquery";
+import "bootstrap";
+import "jquery.easing";
+import DataTable from "datatables.net-bs5";
+import "datatables.net";
+import { Chart } from "chart.js";
 
-setupCounter(document.querySelector("#counter"));
+$(function () {
+  $("#dataTable").DataTable({
+    ajax: {
+      url: "http://localhost:3050/get-data",
+      dataSrc: "", // Use "data" if your endpoint returns { data: [...] }
+    },
+    columns: [
+      { data: "id", title: "ID" },
+      { data: "value", title: "VALUE" },
+      { data: "created_at", title: "CREATED_AT" },
+    ],
+    responsive: true,
+  });
+});
